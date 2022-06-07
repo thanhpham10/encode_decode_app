4f9d109e5f0902f347995b590f044401c9c2ee66
thanhpham10 <92438300+thanhpham10@users.noreply.github.com>
2022-06-07 15:19:02 +0700

    Update encode.py

--- encode.py
+++ encode.py
@@ -8,8 +8,7 @@ window = Tk()
 window.geometry("600x300+300+200")
 window.title("app build by Thanh")
 window.iconbitmap("icon.ico")
-
-
+win.resizable(False, False)
 
 def bt_click():
     text = ety.get()
